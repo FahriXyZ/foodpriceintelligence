ee131be480e8eda372962f6c2a63c788de724d4e
Muhammad Fahri <fahri29mkt@gmail.com>
2026-07-15 17:22:30 +0700

    commit setelah repair code error di vps #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import {
   LayoutDashboard,
   Newspaper,
